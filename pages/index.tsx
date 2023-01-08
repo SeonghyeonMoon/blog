dd55bc0b7aa5cfcd21e4fc3f982abb119d852b01
문성현 <shine7329@gmail.com>
2023-01-08 19:24:53 +0900

    ✨Feature: 포스트 목록 getStaticProps 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,20 @@
 import Head from 'next/head';
+import { fetchPosts } from '@/apis/notion';
+import type { InferGetStaticPropsType } from 'next';
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Title</title>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-    </>
-  );
-}
+const Home = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => (
+  <>
+    <Head>
+      <title>Title</title>
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+    </Head>
+  </>
+);
+
+export default Home;
+
+export const getStaticProps = async () => {
+  const databaseId = process.env.NOTION_DATABASE_ID;
+  const posts = await fetchPosts(databaseId!);
+  return { props: { posts } };
+};
