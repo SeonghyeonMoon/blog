70e65a061e666d8e03762e4e46c77e893f7fdb3f
문성현 <shine7329@gmail.com>
2023-01-08 21:27:11 +0900

    ✨Feature: 다크모드 포함한 포스트 제목 태그 스타일

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,23 @@ const Home = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => (
       <title>Title</title>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
     </Head>
+    <ul>
+      {posts.map(({ id, title, tags }) => (
+        <li key={id}>
+          <h2 className='text-white my-2 text-2xl font-normal'>{title}</h2>
+          <ul className='flex gap-1.5'>
+            {tags.map(({ id, name, color }) => (
+              <li
+                key={id}
+                className={`rounded bg-${color}-light px-1.5 text-font-light dark:bg-${color}-dark dark:text-font-dark`}
+              >
+                {name}
+              </li>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </ul>
   </>
 );
 
