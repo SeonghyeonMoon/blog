8695925fcb83f85910b66b06b25e9f291a692ffd
문성현 <shine7329@gmail.com>
2023-01-09 14:14:37 +0900

    🧹Chore: favicon 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ const Home = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => (
     <Head>
       <title>Title</title>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <link rel='icon' href='/favicon.ico' />
     </Head>
     <ul>
       {posts.map(({ id, title, tags }) => (
