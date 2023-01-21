87f613c71411508ae6428e2028e6d2d16f3668a4
문성현 <shine7329@gmail.com>
2023-01-21 15:41:27 +0900

    🧹Chore: emotion 환경 설정

---
+++ types/emotion.d.ts
@@ -0,0 +1,19 @@
+import '@emotion/react';
+
+declare module '@emotion/react' {
+  export interface Theme {
+    background: string;
+    default: string;
+    font: string;
+    pink: string;
+    red: string;
+    orange: string;
+    yellow: string;
+    green: string;
+    blue: string;
+    purple: string;
+    brown: string;
+    gray: string;
+    hr: string;
+  }
+}
