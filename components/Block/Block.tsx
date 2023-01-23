2071cc1fd805c0d98f703ae4cc191ce30e1518c0
문성현 <shine7329@gmail.com>
2023-01-23 19:03:33 +0900

    ✨Feature: Block Header, Paragraph, List 추가

---
+++ components/Block/Block.tsx
@@ -0,0 +1,55 @@
+import Li from '@/components/Block/Li';
+import P from '@/components/Block/P';
+import Ul from '@/components/Block/Ul';
+import H1 from './H1';
+import H2 from './H2';
+import H3 from './H3';
+import type { BlockType } from '@/apis/notion';
+
+type BlockProps = {
+  block: BlockType;
+};
+
+const Block = ({ block }: BlockProps) => {
+  const { type, text } = block;
+  switch (type) {
+    case 'paragraph':
+      return <P>{text}</P>;
+    case 'heading_1':
+      return <H1>{text}</H1>;
+    case 'heading_2':
+      return <H2>{text}</H2>;
+    case 'heading_3':
+      return <H3>{text}</H3>;
+    case 'bulleted_list_item':
+      return (
+        <Li>
+          {text}
+          {block.children ? (
+            <Ul>
+              {block.children.map((child, index) => (
+                <Block block={child} key={index} />
+              ))}
+            </Ul>
+          ) : null}
+        </Li>
+      );
+    case 'numbered_list_item':
+      return (
+        <Li>
+          {text}
+          {block.children ? (
+            <ol>
+              {block.children.map((child, index) => (
+                <Block block={child} key={index} />
+              ))}
+            </ol>
+          ) : null}
+        </Li>
+      );
+    default:
+      return null;
+  }
+};
+
+export default Block;
