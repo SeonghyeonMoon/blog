4c53e11bbab62c0a191902e85bc880a7dbe0503d
문성현 <shine7329@gmail.com>
2023-01-27 10:11:37 +0900

    feat: Header inner, sticky 스타일 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,39 +11,48 @@ const Header = ({ toggleTheme }: HeaderProps) => {
   return (
     <header
       css={css`
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        max-width: 800px;
-        margin: 0 auto;
-        padding: 20px 0;
+        position: sticky;
+        top: 0;
+        backdrop-filter: blur(5px);
+        z-index: 10;
       `}
     >
-      <Link href='/'>
-        <h1
-          css={css`
-            margin-top: 0;
-          `}
-        >
-          Moon
-        </h1>
-      </Link>
-      <ul
+      <div
         css={css`
           display: flex;
-          gap: 20px;
+          align-items: center;
+          justify-content: space-between;
+          max-width: 800px;
+          margin: 0 auto;
+          padding: 20px 0;
         `}
       >
-        <li>
-          <Link href='/'>Home</Link>
-        </li>
-        <li>
-          <Link href='/about'>About</Link>
-        </li>
-      </ul>
-      <Button onClick={toggleTheme}>
-        <Image src='/moon.png' alt='moon' width={15} height={15} />
-      </Button>
+        <Link href='/'>
+          <h1
+            css={css`
+              margin-top: 0;
+            `}
+          >
+            Moon
+          </h1>
+        </Link>
+        <ul
+          css={css`
+            display: flex;
+            gap: 20px;
+          `}
+        >
+          <li>
+            <Link href='/'>Home</Link>
+          </li>
+          <li>
+            <Link href='/about'>About</Link>
+          </li>
+        </ul>
+        <Button onClick={toggleTheme}>
+          <Image src='/moon.png' alt='moon' width={15} height={15} />
+        </Button>
+      </div>
     </header>
   );
 };
