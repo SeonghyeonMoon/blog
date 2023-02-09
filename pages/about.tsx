3db2638e594257a0bbbc94a167db7ff4d185cd58
문성현 <shine7329@gmail.com>
2023-02-09 19:44:24 +0900

    chore: about 컴포넌트 index.ts 통해서 export

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,5 @@
 import Head from 'next/head';
-import Certification from '@/components/About/Certification';
-import Education from '@/components/About/Education';
-import Info from '@/components/About/Info';
-import SingleProject from '@/components/About/SingleProject';
-import TeamProject from '@/components/About/TeamProject';
+import { Certification, Education, Info, SingleProject, TeamProject } from '@/components/About';
 
 const About = () => {
   return (
